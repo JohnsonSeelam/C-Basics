032e7a0459dd5657c1e800a6c3ea15304dac7603
JohnsonSeelam <69499718+JohnsonSeelam@users.noreply.github.com>
2021-10-27 10:39:49 +0530

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	
+	//variable declaration
+	int a,b,sum;
+	
+	//initialisation
+	a=10;
+	b=20;
+	sum=a+b;
+	
+	//output
+	printf("Sum of %d and %d is:%d",a,b,sum);
+}
