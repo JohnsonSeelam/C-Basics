63e63929ff56230b8ae653cc8243da10a4f3318b
JohnsonSeelam <69499718+JohnsonSeelam@users.noreply.github.com>
2021-11-04 21:18:34 +0530

    Add files via upload

---
+++ even_odd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int n=5,N,E,O;
+	N=n*(n+1)/2;
+	E=n*(n+1);
+	O=n+n+1;
+	printf("Sum of N natural number is %d\n",N);
+	printf("Sum of N even number is %d\n",E);
+	printf("Sum of N odd number is %d\n",O);
+}
